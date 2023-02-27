acc13156f3200ab6ed89706ab91ddf4c11c12813
just2102 <thesixteensaltines@gmail.com>
2023-02-27 13:08:33 +0900

    replace browserrouter with hashrouter for gh-pages to work correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+
 import App from './App'
 import './index.css'
 import { store } from './Redux/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
     <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
